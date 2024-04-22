cc79aa45b61091eea14f3843e67fddbf353cd480
anayethasan <mdneloy256@gmail.com>
2024-04-23 02:25:40 +0600

    js and responsice part added

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 
 // active navbar
+let nav = document.querySelector(".navigation-wrap");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
 
 
 // navbar hide
-
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
 
 // counter design
 document.addEventListener("DOMContentLoaded", () => {
